60aa4362a692465ddcb112a81f0636f7d5ff9634
Guillermo Valentín <masweb@me.com>
2024-05-18 12:50:27 +0200

    init 9

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Intro from '@/components/Intro.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VUE_APP_BASE_URL),
   routes: [
     {
       path: '/',
@@ -12,9 +12,6 @@ const router = createRouter({
     {
       path: '/cv',
       name: 'cv',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('@/components/Cv.vue')
     }
   ]
