c257042e4165a63218dabcd4867f413ad23958c7
Guillermo Valentín <masweb@me.com>
2025-08-13 00:12:21 +0200

    pic

--- src/components/Cv.vue
+++ src/components/Cv.vue
@@ -6,16 +6,14 @@ import { NGrid, NGridItem } from 'naive-ui'
 onMounted(() => {
   document.title = 'CV. Guillermo Valentín Sánchez'
 })
-
 </script>
 
 <template>
-
   <div class="container mb5">
     <n-grid class="mt3" cols="1 s:1 m:2 " x-gap="10" responsive="screen">
       <n-grid-item>
         <div class="flex justify-end">
-          <img class="pic" src="/pic.jpeg" />
+          <img class="pic" src="/IMG_4663RED.png" />
         </div>
       </n-grid-item>
       <n-grid-item class="letter">
@@ -23,62 +21,77 @@ onMounted(() => {
         <p class="mt0 mb0"><strong>Desarrollador front end senior</strong></p>
         <p class="mt0 mb0"><strong>Diseñador gráfico</strong></p>
         <p class="mt0">nacimiento: 1973</p>
-        <p class="text">Hola!, desde el año 1992 he trabajado en entornos de edición y publicidad, como diseñador gráfico, aunque ahora ya no ejerzo,
-          definitivamente esa
-          experiencia es muy útil para realizar de manera óptima mis tareas como maquetador web y desarrollador front-end.</p>
-        <p class="text">Soy entusiasta de las tecnologías y desarrollo de software y autodidacta, llevo codificando profesionalmente desde 2012. Transicioné el
-          paso de
-          jQuery para todo, a <strong>JavaScript moderno</strong> en entornos profesionales. </p>
-        <p class="text">Soy una persona <a target="_blank" href="https://es.wikipedia.org/wiki/Sensibilidad_de_procesamiento_sensorial">SPS</a> con altas
-          capacidades, utilizo el proceso intensivo, capacidad de análisis, cuidado de los detalles... para prácticamente todo, puedo comprender, aprender y abordar
-          temáticas complejas en todos los ámbitos en muy poco tiempo.</p>
-        <p class="text">En los últimos años he sido partícipe en un porcentaje muy alto en un proyecto realmente grande para gestión de eventos donde he ideado
-          y producido un editor web desde 0 totalmente operativo, esto me ha permitido llevar las tecnologías al límite y conocer los mejores métodos, técnicas y librerías
-          para desarrollo front-end, además de habituarme al trabajo en equipo con <strong>Jira</strong>, <strong>Git Flow</strong>.</p>
-        <p class="text">Desarrollo de grandes proyectos, apps nocode, podría decir que soy experto en <strong>Vue</strong> 3, <strong>Typescript</strong> y
-          <strong>Nuxt</strong> 3
-          al realizar una gran cantidad de proyectos profesionales en este ecosistema (también en las versiones 2). Podría crear y adaptarme con cierta
-          facilidad proyectos de <strong>React</strong> y <strong>Angular</strong>.</p>
-        <p class="text">Además, en los últimos tiempos estoy realizando proyectos backend en GO, estupendo lenguaje para realización de proyectos de alto rendimiento,
-          escalable, seguro y de desarrollo rápido.</p>
+        <p class="text">
+          Hola!, desde el año 1992 he trabajado en entornos de edición y publicidad, como diseñador gráfico, aunque
+          ahora ya no ejerzo, definitivamente esa experiencia es muy útil para realizar de manera óptima mis tareas como
+          maquetador web y desarrollador front-end.
+        </p>
+        <p class="text">
+          Soy entusiasta de las tecnologías y desarrollo de software y autodidacta, llevo codificando profesionalmente
+          desde 2012. Transicioné el paso de jQuery para todo, a <strong>JavaScript moderno</strong> en entornos
+          profesionales.
+        </p>
+        <p class="text">
+          Soy una persona
+          <a target="_blank" href="https://es.wikipedia.org/wiki/Sensibilidad_de_procesamiento_sensorial">SPS</a> con
+          altas capacidades, utilizo el proceso intensivo, capacidad de análisis, cuidado de los detalles... para
+          prácticamente todo, puedo comprender, aprender y abordar temáticas complejas en todos los ámbitos en muy poco
+          tiempo.
+        </p>
+        <p class="text">
+          En los últimos años he sido partícipe en un porcentaje muy alto en un proyecto realmente grande para gestión
+          de eventos donde he ideado y producido un editor web desde 0 totalmente operativo, esto me ha permitido llevar
+          las tecnologías al límite y conocer los mejores métodos, técnicas y librerías para desarrollo front-end,
+          además de habituarme al trabajo en equipo con <strong>Jira</strong>, <strong>Git Flow</strong>.
+        </p>
+        <p class="text">
+          Desarrollo de grandes proyectos, apps nocode, podría decir que soy experto en <strong>Vue</strong> 3,
+          <strong>Typescript</strong> y <strong>Nuxt</strong> 3 al realizar una gran cantidad de proyectos profesionales
+          en este ecosistema (también en las versiones 2). Podría crear y adaptarme con cierta facilidad proyectos de
+          <strong>React</strong> y <strong>Angular</strong>.
+        </p>
+        <p class="text">
+          Además, en los últimos tiempos estoy realizando proyectos backend en GO, estupendo lenguaje para realización
+          de proyectos de alto rendimiento, escalable, seguro y de desarrollo rápido.
+        </p>
 
-        <p class="text"><strong>Reutilización de componentes</strong>, <strong>estudio de la arquitectura</strong>, <strong>buenas prácticas</strong>, <strong>código
-          legible y mantenible</strong>, <strong>patrones de diseño</strong>, <strong>pruebas unitarias</strong> y <strong>end to end</strong>
-          . Configuración y manejo de <strong>Git</strong>, <strong>GitHub</strong>, <strong>GitLab</strong>, <strong>Docker</strong>, <strong>IA para programar</strong>. <strong>Usabilidad</strong>, <strong>interfaces gráficas</strong>,
-          <strong>WYSIWYG</strong>,
-          <strong>accesibilidad</strong>, <strong>multi idioma</strong>, <strong>CSS</strong>, <strong>SCSS</strong> 12 años experiencia.</p>
-        <p class="text">Desarrollo de aplicaciones completas, backend GO - Gin o en PHP - Laravel o Typescript - Adonis JS. Comercio electrónico, backoffices
-          personalizados, aplicaciones móviles y de escritorio. Interesado en el la publicación de IA para usuario final, WASM y GO.</p>
+        <p class="text">
+          <strong>Reutilización de componentes</strong>, <strong>estudio de la arquitectura</strong>,
+          <strong>buenas prácticas</strong>, <strong>código legible y mantenible</strong>,
+          <strong>patrones de diseño</strong>, <strong>pruebas unitarias</strong> y <strong>end to end</strong> .
+          Configuración y manejo de <strong>Git</strong>, <strong>GitHub</strong>, <strong>GitLab</strong>,
+          <strong>Docker</strong>, <strong>IA para programar</strong>. <strong>Usabilidad</strong>,
+          <strong>interfaces gráficas</strong>, <strong>WYSIWYG</strong>, <strong>accesibilidad</strong>,
+          <strong>multi idioma</strong>, <strong>CSS</strong>, <strong>SCSS</strong> 12 años experiencia.
+        </p>
+        <p class="text">
+          Desarrollo de aplicaciones completas, backend GO - Gin o en PHP - Laravel o Typescript - Adonis JS. Comercio
+          electrónico, backoffices personalizados, aplicaciones móviles y de escritorio. Interesado en el la publicación
+          de IA para usuario final, WASM y GO.
+        </p>
       </n-grid-item>
-
     </n-grid>
 
-
     <n-grid class="mt0" cols="1 s:1 m:2 " x-gap="10" responsive="screen">
-
       <n-grid-item>
         <h3 class="mb0"><strong>Formación</strong></h3>
         <p class="mb0"><b>Centro de estudios de informática superior Alicante.</b></p>
-        <p class="mt0">Programador de gestión - Analista programador 1990 – 1992. </p>
-
-
+        <p class="mt0">Programador de gestión - Analista programador 1990 – 1992.</p>
       </n-grid-item>
       <n-grid-item>
         <h3 class="mb2"><strong>Autodidacta</strong></h3>
-        <p class="mt0">Mi formación se basa en el seguimiento, testeo y aprendizage mediante artículos, cursos, libros, etc...
-          de todas las tecnologías susceptibles de mejorar la eficiencia en el desarrollo de software. </p>
+        <p class="mt0">
+          Mi formación se basa en el seguimiento, testeo y aprendizage mediante artículos, cursos, libros, etc... de
+          todas las tecnologías susceptibles de mejorar la eficiencia en el desarrollo de software.
+        </p>
         <h3 class="mb0"><strong>Inglés</strong></h3>
         <p class="mb0">Leo y es escribo pero no hablo ni entiendo con facilidad.</p>
       </n-grid-item>
-
     </n-grid>
-
 
     <h3 class="mt3 mb0"><strong>Experiencia</strong></h3>
 
-
     <n-grid class="mt0" cols="1 s:1 m:3 " x-gap="10" responsive="screen">
-
       <n-grid-item>
         <p class="mb0"><b>Bmotion</b></p>
         <p class="mt0 mb0">Desarrollo aplicaciones nocode</p>
@@ -94,7 +107,7 @@ onMounted(() => {
 
         <p class="mb0"><b>NovoSpain</b></p>
         <p class="mt0 mb0">Desarrollo web, diseño gráfico</p>
-        <p class="mt0  mb2">nov.2016 • ene.2020 • Elche</p>
+        <p class="mt0 mb2">nov.2016 • ene.2020 • Elche</p>
       </n-grid-item>
 
       <n-grid-item>
@@ -112,9 +125,8 @@ onMounted(() => {
 
         <p class="mb0"><b>MASA International</b></p>
         <p class="mt0 mb0">Diseñador Gráfico (encargado dept.)</p>
-        <p class="mt0  mb2">jun.2005 • sept.2008 • Torrevieja</p>
+        <p class="mt0 mb2">jun.2005 • sept.2008 • Torrevieja</p>
       </n-grid-item>
-
 
       <n-grid-item>
         <p class="mb0 mt1"><b>Imprenta Hnos. Rastoll</b></p>
@@ -142,7 +154,10 @@ onMounted(() => {
         <p class="mb2 mt2">Autenticación.</p>
         <p class="mb2 mt2">Manejo y conversión de fechas.</p>
         <p class="mb2 mt2">Creación programática y previsualización de PDF.</p>
-        <p class="mb2 mt2">Carruseles, galerías, modal y sliders responsive, tooltips, notificaciones, arrastrar y soltar, calendario interactivo.</p>
+        <p class="mb2 mt2">
+          Carruseles, galerías, modal y sliders responsive, tooltips, notificaciones, arrastrar y soltar, calendario
+          interactivo.
+        </p>
         <p class="mb1 mt2">Validación front-end y back-end en vivo.</p>
       </n-grid-item>
       <n-grid-item>
@@ -166,18 +181,19 @@ onMounted(() => {
         <p class="mb2 mt2">TypeScript.</p>
         <p class="mb1 mt2">Arquitectura hexagonal.</p>
         <p class="mb2 mt2">Motores de plantillas (pug, blade, liquid...).</p>
-        <p class="mb2 mt2">Gestión de estado, Pinia.. </p>
+        <p class="mb2 mt2">Gestión de estado, Pinia..</p>
         <p class="mb2 mt2">Prototipado UI-UX.</p>
         <p class="mb1 mt2">Flutter, Tauri, Electron, App híbrida, Cordova, Ionic. App nativo.</p>
       </n-grid-item>
     </n-grid>
 
-
     <h3 class="mb0"><strong>Backend</strong></h3>
     <n-grid class="mt0" cols="1 s:1 m:3 " x-gap="10" responsive="screen">
       <n-grid-item>
         <p class="mb2 mt1">Creación de backend API Rest o con plantillas con GO/Gin, TypeScript/Adonis, PHP/Laravel</p>
-        <p class="mb2 mt1">Creación, diseño y optimización de bases de datos relacionales MySql, MariaDB, Postgress...</p>
+        <p class="mb2 mt1">
+          Creación, diseño y optimización de bases de datos relacionales MySql, MariaDB, Postgress...
+        </p>
         <p class="mb2 mt2">Migraciones y generación de datos de prueba.</p>
         <p class="mb2 mt2">Login y registro seguro, encriptación.</p>
         <p class="mb1 mt2">Gestión y proceso de ficheros desde el servidor, imagen, vectorial, excel, pdf ...</p>
@@ -196,8 +212,6 @@ onMounted(() => {
         <p class="mb2 mt2">ORM Gorm, Eloquent y Lucid.</p>
       </n-grid-item>
     </n-grid>
-
-
   </div>
 
   <div class="mb5">&nbsp;</div>
@@ -210,6 +224,4 @@ onMounted(() => {
   <div class="mb5">&nbsp;</div>
   <div class="mb5">&nbsp;</div>
   <div class="mb5">&nbsp;</div>
-
-
 </template>
