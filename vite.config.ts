9458d22d5d23fcc347c256332f0dff8ba1d04979
Guillermo Valentín <masweb@me.com>
2025-12-16 15:32:56 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,42 +2,47 @@ import vue from '@vitejs/plugin-vue'
 import { URL, fileURLToPath } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { type Plugin, defineConfig } from 'vite'
+import { type Plugin, defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      dts: 'src/auto-imports.d.ts',
-      imports: ['vue', 'pinia', 'vue-router', 'vue-i18n', 'vee-validate'],
-      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/],
-      dirs: [
-        'src/composables/**',
-        'src/plugins/**',
-        'src/services/**',
-        'src/utils/**',
-        'src/types/**',
-      ],
-    }) as Plugin,
-    Components({
-      dirs: ['src/services/**', 'src/components/**'],
-    }) as Plugin,
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
-        quietDeps: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_APP_BASE_URL || '/',
+    plugins: [
+      vue(),
+      AutoImport({
+        dts: 'src/auto-imports.d.ts',
+        imports: ['vue', 'pinia', 'vue-router', 'vue-i18n', 'vee-validate'],
+        include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/],
+        dirs: [
+          'src/composables/**',
+          'src/plugins/**',
+          'src/services/**',
+          'src/utils/**',
+          'src/types/**',
+        ],
+      }) as Plugin,
+      Components({
+        dirs: ['src/services/**', 'src/components/**'],
+      }) as Plugin,
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
-  optimizeDeps: {
-    include: ['@coreui/coreui', '@coreui/vue', '@tabler/icons-vue'],
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
+          quietDeps: true,
+        },
+      },
+    },
+    optimizeDeps: {
+      include: ['@coreui/coreui', '@coreui/vue', '@tabler/icons-vue'],
+    },
+  }
 })
